b51cb1c9a2175df6ab9c5112629abe77d51d0906
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-01 16:09:52 +0100

    configure fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        dm: ["DM Sans"],
+        open: ["Open Sans"],
+      },
+    },
   },
   plugins: [],
 };
