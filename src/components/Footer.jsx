7abaf2abd7bd75efd34b6247bf37f20ddb7063a9
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-13 15:47:34 +0000

    updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     return (
  <div className=" relative p-5 mx-auto font-dm py-10">
     <div className="flex gap-4  grid-cols-12 grid ">
-      <div className="bg-black col-span-4 text-white rounded-[3rem] flex flex-col gap-5 p-8 ">
+      <div className="bg-black col-span-4 text-white rounded-[2.5rem] flex flex-col justify-between gap-5 p-8 ">
         <p className="text-3xl font-bold">
           crafting unique user interfaces for the web
         </p>
@@ -18,7 +18,8 @@ const Footer = () => {
             <Button icon={<MdArrowOutward/>}>shop now</Button>
           
       </div>
-      <div className="col-span-8 gap-20 bg-[#d9d9d9]  rounded-[3rem] flex p-8">
+      <div className="col-span-8 bg-[#d9d9d9]  rounded-[2.5rem] p-8 pb-2">
+        <div className="flex gap-20">
         <div className="flex flex-col gap-4">
         <p className="text-lg capitalize">links</p>
           <div className="flex flex-col gap-2 text-[#0e0e0e]/80">
@@ -43,19 +44,23 @@ const Footer = () => {
          
         </div>
         <div className="flex flex-1 justify-end  gap-2 h-max items-center">
-         <img src="panda.svg" className="h-[80px]"/>
+        <img src="pandaemoji.svg" className="h-[60px]"/>
+         <img src="panda.svg" className="h-[60px]"/>
           
         </div>
-      </div>
-    </div>
-    <div className="flex justify-between text-xs py-6 col-span-7 items-center capitalize">
-      <p>&copy;2023 HarvestGrove</p>
+        </div>
+        <div className="flex justify-between py-6 col-span-7 items-center ">
+      <p>&copy;2024 uipanda</p>
       <div className="flex items-center gap-2">
         <a href="/">privacy policy</a>
-        <div className="w-2 h-2 rounded-full bg-lime-200"></div>
+        <div className="w-2 h-2 rounded-full bg-[#0e0e0e]"></div>
         <a href="/">terms of use</a>
       </div>
     </div>
+       
+      </div>
+    </div>
+  
   </div>)
 }
 
