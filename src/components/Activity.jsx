277a2a8b649fadca8137666da848cd8907e8ffd6
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2024-01-14 11:31:57 +0000

    card updates

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -3,7 +3,7 @@ import { BsArrowUpCircle } from "react-icons/bs";
 import { SlGraph, SlPlus } from "react-icons/sl";
 const Activity = () => {
     return(
-        <div className="font-poppins flex flex-col gap-10 m-4 bg-[#0e0e0e] rounded-3xl text-white p-5 w-max">
+        <div className="font-poppins flex flex-col gap-10 m-4 bg-[#0e0e0e] rounded-[2.5rem] text-white p-5 w-max">
             <div className="flex justify-between items-center">
                 <p className="capitalize text-3xl">activity</p>
                 <div className="p-3 bg-[#d9d9d9]/50 rounded-full">
@@ -11,15 +11,15 @@ const Activity = () => {
                 </div>
             </div>
             <div className="flex gap-8">
-<div className="">
-    <p className="text-3xl"><BsArrowUpCircle className="inline-block mr-1"/>37<span className="text-lg opacity-80">%</span></p>
+<div className="flex flex-col items-center">
+    <p className="text-3xl"><BsArrowUpCircle className="inline-block mr-1"/>37<span className="text-lg opacity-80"> %</span></p>
     <p className="opacity-50 text-sm py-1">vs yesterday</p>
 </div>
-<div className="">
-    <p className="text-3xl"><SlPlus className="inline-block mr-1"/>935<span className="text-lg opacity-80">kcal</span></p>
+<div className="flex flex-col items-center">
+    <p className="text-3xl"><SlPlus className="inline-block mr-1"/>935<span className="text-lg opacity-80"> kcal</span></p>
     <p className="opacity-50 text-sm py-1">total burned</p>
-</div><div className="">
-    <p className="text-3xl"><SlGraph className="inline-block mr-1"/>327<span className="text-lg opacity-80">kcal</span></p>
+</div><div className="flex flex-col items-center">
+    <p className="text-3xl"><SlGraph className="inline-block mr-1"/>327<span className="text-lg opacity-80"> kcal</span></p>
     <p className="opacity-50 text-sm py-1">daily avg.</p>
 </div>
 
@@ -44,19 +44,21 @@ const Activity = () => {
                 </div>
             </div>
             <div className="relative w-full h-3 rounded-full bg-[#d9d9d9]/20">
-                <div className="absolute bg-[#fff] w-[30%] rounded-full h-full left-0"></div>
+                <div className="absolute bg-[#fff] w-[43%] rounded-full h-full left-0"></div>
+                <div className="absolute bg-[#fff]/60 w-[67%] rounded-full h-full left-0"></div>
+                <div className="absolute bg-[#fff]/40 w-[89%] rounded-full h-full left-0"></div>
             </div>
-            <div className="flex gap-8">
-<div className="">
-    <p className="text-3xl"><BsArrowUpCircle className="inline-block mr-1"/>37<span className="text-lg opacity-80">%</span></p>
-    <p className="opacity-50 text-sm py-1">vs yesterday</p>
+            <div className="flex justify-between gap-8">
+<div className="flex flex-col items-center">
+    <p className="text-3xl">402<span className="text-lg opacity-80"> cal</span></p>
+    <p className="text-[#d9d9d9]/60 text-sm py-1"><div className="w-2 h-2 rounded-full bg-[#fff] inline-block mr-1"></div>running . 43%</p>
 </div>
-<div className="">
-    <p className="text-3xl"><SlPlus className="inline-block mr-1"/>935<span className="text-lg opacity-80">kcal</span></p>
-    <p className="opacity-50 text-sm py-1">total burned</p>
-</div><div className="">
-    <p className="text-3xl"><SlGraph className="inline-block mr-1"/>327<span className="text-lg opacity-80">kcal</span></p>
-    <p className="opacity-50 text-sm py-1">daily avg.</p>
+<div className="flex flex-col items-center">
+    <p className="text-3xl">302<span className="text-lg opacity-80"> cal</span></p>
+    <p className="text-[#d9d9d9]/60 text-sm py-1"><div className="w-2 h-2 rounded-full bg-[#fff]/60 inline-block mr-1"></div>walking . 24%</p>
+</div><div className="flex flex-col items-center">
+    <p className="text-3xl">215<span className="text-lg opacity-80"> cal</span></p>
+    <p className="text-[#d9d9d9]/60 text-sm py-1"><div className="w-2 h-2 rounded-full bg-[#fff]/40 inline-block mr-1"></div>cycling . 22%</p>
 </div>
 
             </div>
