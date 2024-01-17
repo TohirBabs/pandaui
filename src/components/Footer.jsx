f5341863910fe695aa9fb727dc2232518803e5d4
panda <tohirbabs@gmail.com>
2024-01-17 16:55:22 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { MdArrowOutward } from "react-icons/md";
 
 const Footer = () => {
     return (
- <div className=" relative lg:p-5 p-1 mx-auto font-dm py-10">
+    <div className=" relative lg:p-5 p-1 mx-auto font-dm py-10">
     <div className="flex flex-col lg:gap-4 gap-2 lg:grid-cols-12 lg:grid ">
       <div className="bg-black lg:col-span-4 text-white rounded-[2.5rem] flex flex-col justify-between gap-5 p-6 lg:p-8 ">
         <p className="text-3xl font-bold">
@@ -15,8 +15,7 @@ const Footer = () => {
           stewards of excellence, where dedication to innovation, creativity
           and sustainable farming practices flourishes.
         </p>
-            <Button icon={<MdArrowOutward/>}>shop now</Button>
-          
+        <Button icon={<MdArrowOutward/>}>shop now</Button>          
       </div>
       <div className="lg:col-span-8 bg-[#d9d9d9]  rounded-[2.5rem] lg:p-8 p-6 pb-2">
         <div className="flex flex-col-reverse lg:flex-row  lg:gap-20 gap-5">
@@ -41,13 +40,11 @@ const Footer = () => {
         <div className="flex flex-col gap-2 text-[#0e0e0e]/80">
           {["FAQs","contact","help center"].map((link, index)=> 
             <Link key={index} href="/" variant="underline">{link}</Link>)}
-          </div>
-         
+          </div>        
         </div></div>
         <div className="flex flex-1 lg:justify-end  gap-2 h-max items-center">
         <img src="pandaemoji.svg" className="lg:h-[60px] h-[40px]"/>
-         <img src="panda.svg" className="lg:h-[60px] h-[40px]"/>
-          
+         <img src="panda.svg" className="lg:h-[60px] h-[40px]"/>         
         </div>
         </div>
         <div className="flex justify-between py-6 col-span-7 items-center ">
