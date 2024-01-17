1d0dddabfb3c9d0bbcf780090b0d1018a5b6091c
panda <tohirbabs@gmail.com>
2024-01-17 16:55:45 +0100

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Loader = () => {
   return (
-    <div>Loader</div>
+      <div className='flex justify-center items-center gap-2 p-10'>
+        <div className='w-4 h-4 rounded-full bg-[#d9d9d9]'></div>
+        <div className='w-4 h-4 rounded-full bg-[#d9d9d9]'></div>
+        <div className='w-4 h-4 rounded-full bg-[#d9d9d9]'></div>
+        <div className='w-4 h-4 rounded-full bg-[#d9d9d9]'></div>
+        <div className='w-4 h-4 rounded-full bg-[#d9d9d9]'></div>
+    </div>
   )
 }
 
