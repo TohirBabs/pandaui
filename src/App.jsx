cee052104c45f7f359c9312888b0b5f2a9e81ec8
panda <tohirbabs@gmail.com>
2024-01-17 16:53:08 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Activity from "./components/Activity"
+import { Clock } from "./components/Clock"
 import Footer from "./components/Footer"
 import { Gallery } from "./components/Gallery"
+import Loader from "./components/Loader"
 
 const App = () => {
-  return (<Gallery/>)
+  return (<div className="w-screen min-h-screen bg-[#0e0e0e] flex items-center font-mono justify-center"><Clock/></div>)
 }
 
 export default App
