e4d43d8cf7aff199ee876696e3f40d2355fdf0d0
panda <tohirbabs@gmail.com>
2024-01-18 18:09:01 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import Activity from "./components/Activity"
-import { Clock } from "./components/Clock"
-import Footer from "./components/Footer"
-import { Gallery } from "./components/Gallery"
-import Loader from "./components/Loader"
+import { Battery } from "./components/Battery"
+import { Time } from "./components/Time"
+
 
 const App = () => {
-  return (<div className="w-screen min-h-screen bg-[#0e0e0e] flex items-center font-mono justify-center"><Clock/></div>)
+  return (<div className="w-screen min-h-screen bg-[#fff] flex-col flex items-center font-mono justify-center">
+    <img src="/pandaemoji.svg" className="p-3 h-16 w-16 rounded-xl bg-[#0e0e0e]/70 m-4" />
+    <h2 className="text-2xl text-center">minimalistic web widgets </h2>
+
+    <div className="grid grid-cols-12 gap-6 w-full p-6">
+      <Time/>
+      <Battery />
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-6 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-6 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+      <div className="col-span-3 h-[20vw] bg-[#0e0e0e] rounded-3xl"></div>
+
+
+    </div>
+  </div>)
 }
 
 export default App
