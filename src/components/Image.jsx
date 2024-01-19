a255250c964773e458410095b7ca168f0fe3e87f
panda <tohirbabs@gmail.com>
2024-01-19 16:14:53 +0100

    imamge

---
+++ src/components/Image.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Image = () => {
+  return (
+            <div className="md:col-span-3 col-span-6 md:h-[20vw] h-[40vw] relative overflow-hidden font-bold bg-[#0e0e0e] md:rounded-[2.4rem] rounded-3xl text-white flex items-center justify-center ">
+   image
+          </div>
+
+  )
+}
+
+export default Image
