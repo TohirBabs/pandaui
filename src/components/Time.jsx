03e318bff8cb7d5f077afcdc891b81e58e04c965
panda <tohirbabs@gmail.com>
2024-01-23 13:38:18 +0100

    time

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 
-export const Time = () => {
+export const Time = ({mobileView}) => {
  const minutesArray = Array.from({ length: 60 }, (_, index) => index < 10? "0"+index : index);
 
     const [time, setTime] = useState(new Date());
@@ -35,12 +35,10 @@ export const Time = () => {
               <div className='flex flex-col gap-2 justify-center  relative  transition-all'>
                   <div className='absolute md:h-[230px] h-[40vw] w-full bg-gradient-to-b from-[#0e0e0e] via-transparent to-[#0e0e0e] z-20'></div>
           <div
-            className='flex relative flex-col w-[90px] h-[72px]'>
+            className='flex relative flex-col md:w-[90px] md:h-[72px] w-[59px] h-[48px]'>
             <div       
-              style={{ transform: `translateY(${minutes * -72}px)` }}
-              // style={{ top: `-72px` }}
-
-              className="flex transition-all duration-500 transition flex-col absolute">
+              style={{ transform: `translateY(${mobileView ? minutes * -48 : minutes * -72}px)` }}
+              className={`flex transition-all duration-500 transition flex-col absolute `}>
                   {minutesArray.map((minute) => <span  key={minute}>{minute}</span>)}</div></div>
         </div>
       </div>
