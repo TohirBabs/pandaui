eb54684efcc8e1078e6f01e8406c84d13a224a21
panda <tohirbabs@gmail.com>
2024-01-23 13:38:10 +0100

    battery

--- src/components/Battery.jsx
+++ src/components/Battery.jsx
@@ -29,7 +29,8 @@ export const Battery = () => {
       
     navigator.getBattery()
     .then(function(battery) {
-        // Get current battery level .
+      // Get current battery level .
+      console.log(battery.level);
         setBatteryLevel(battery.level * 100);
         setChargeOrDischargeTime(battery.charging ? battery.chargingTime : battery.dischargingTime)
         setCharging(battery.charging)
@@ -45,16 +46,18 @@ export const Battery = () => {
   }, []);
 }
 else{
-    // if battery API is not working
+      // if battery API is not working
+      console.log("no battery api");
 }
-
+console.log(batteryLevel);
   return (
       <div className="md:col-span-3 col-span-6 md:h-[230px] h-[40vw] relative overflow-hidden  bg-[#0e0e0e] rounded-3xl text-white flex items-center justify-center ">
           <div className='flex-col items-center w-full h-full justify-center flex gap-3'>
-              <span className='md:text-6xl text-3xl text-center font-bold'>{batteryLevel}%</span>
-              <div className='h-[28%] w-[70%] md:rounded-xl rounded-lg bg-[#d9d9d9]/50 overflow-hidden flex items-center justify-center relative'>
-                  <div style={{width:`${batteryLevel}%`}} className='h-full absolute left-0 transition-all bg-[#d9d9d9]'></div>
-                  { charging && <BsLightningChargeFill className='z-10 md:text-3xl text-xl text-[#0e0e0e]'/>}
+              <span className='md:text-6xl text-3xl text-center font-bold'>{Math.round(batteryLevel)}%</span>
+              <div className='h-[30%] w-[70%] md:rounded-xl rounded-lg bg-[#d9d9d9]/50  flex items-center justify-center relative'>
+          <div style={{ width: `${batteryLevel}%` }} className='h-full absolute rounded-lg md:rounded-xl left-0 transition-all bg-[#d9d9d9]'></div>
+          <div className='absolute md:h-8 h-4 rounded-r-sm md:w-2 w-1 right bg-[#d9d9d9]/50 md:-right-2 -right-1'></div>
+                  { charging && <BsLightningChargeFill className='z-10 md:text-4xl text-2xl text-[#0e0e0e]'/>}
 
               </div>
               <span className='text-base text-sm text-center'>{formatTime(hours)}hrs {formatTime(minutes)}mins left</span>
