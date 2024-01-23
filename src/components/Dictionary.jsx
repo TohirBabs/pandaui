13e834426231a42f58bc802e49d4f30d820f27cc
panda <tohirbabs@gmail.com>
2024-01-23 09:56:17 +0100

    diction

--- src/components/Dictionary.jsx
+++ src/components/Dictionary.jsx
@@ -3,7 +3,7 @@ import { HiSpeakerWave } from "react-icons/hi2";
 
 export const Dictionary = () => {
     return (
-<div className="md:col-span-6 col-span-12 md:h-[20vw] h-[40vw] relative overflow-hidden  bg-[#0e0e0e] md:rounded-[2.4rem] rounded-3xl text-white flex items-center justify-center ">
+<div className="md:col-span-6 col-span-12 md:h-[220px] h-[40vw] relative overflow-hidden  bg-[#0e0e0e] rounded-3xl text-white flex items-center justify-center ">
             <div className='flex-col md:p-8 p-4 justify-center w-full h-full flex md:gap-4 gap-2'>
                 <div className='flex justify-between w-full items-center '>
                     <h3 className='md:text-5xl text-3xl font-bold'>perfect</h3>
