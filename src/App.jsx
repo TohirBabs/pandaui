fda7ad6af932bdcdd27a932e194b8f9fab699fe4
panda <tohirbabs@gmail.com>
2024-01-23 13:53:23 +0100

    app

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,36 @@ import { Dictionary } from "./components/Dictionary"
 import Image from "./components/Image"
 import { Time } from "./components/Time"
 import { Weather } from "./components/Weather"
+import { useEffect } from "react"
+import { useState } from "react"
 
 
 const App = () => {
 
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
 
+    const [time, setTime] = useState(new Date());
+
+  const hours = time.getHours();
+  const minutes = time.getMinutes();
+ 
+
+  useEffect(() => {
+    // Update the time every second
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(interval);
+  }, []);
+
   return (<div className="w-screen min-h-screen bg-[#fff] flex-col flex items-center font-mono justify-center">
     <img src="/pandaemoji.svg" className="p-3 h-16 w-16 rounded-xl bg-[#0e0e0e]/70 m-4" />
     <h2 className="md:text-2xl text-lg text-center">minimalistic web widgets </h2>
 
     <div className="grid grid-cols-12 md:gap-[2rem] gap-[1rem] max-w-6xl p-[0.5rem]">
-      <Time mobileView={ windowSize.current[0] < 768}/>
+      <Time mobileView={windowSize.current[0] < 768} time={ {hours:hours,minutes:minutes}} />
       <Battery />
       <Weather/>
       <Dictionary />
